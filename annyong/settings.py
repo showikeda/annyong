cde9750335f640902dd0e612b831e6cc023bb651
showikeda <ikeikedunk@gmail.com>
2020-06-10 01:34:29 +0900

    makemigrations migrate sns

--- annyong/settings.py
+++ annyong/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'annyong.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
