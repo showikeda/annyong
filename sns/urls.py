76bf25a7a3a3d634e7161e2bee324b375c0d301a
showikeda <ikeikedunk@gmail.com>
2020-06-12 01:25:25 +0900

    add detail html

--- sns/urls.py
+++ sns/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'sns'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:id>', views.detail, name='detail'),
 ]
--- sns/views.py
+++ sns/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Article
 
 def index(request):
-    return render(request, 'sns/index.html')
+    articles = Article.objects.all()
+    context = {
+        'message': 'Welcome my BBS',
+        'articles': articles,
+    }
+    return render(request, 'sns/index.html', context)
+
+def detail(request, id):
+    article = get_object_or_404(Article, pk=id)
+    context = {
+        'message': 'Show Article' + str(id),
+        'article': article,
+    }
+    return render(request, 'sns/detail.html', context)
