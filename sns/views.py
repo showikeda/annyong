ac4c09a12c106a04c9f2922ec14955a97c39c04f
showikeda <ikeikedunk@gmail.com>
2020-06-12 21:28:16 +0900

    add create delete

--- sns/views.py
+++ sns/views.py
@@ -17,3 +17,25 @@ def detail(request, id):
         'article': article,
     }
     return render(request, 'sns/detail.html', context)
+
+def create(request):
+    article = Article(content='Hello BBS', user_name='paiza')
+    article.save()
+    articles = Article.objects.all()
+
+    context = {
+        'message': 'Create article',
+        'articles': articles,
+    }
+    return render(request, 'sns/index.html', context)
+
+def delete(request, id):
+    article = get_object_or_404(Article, pk=id)
+    article.delete()
+    articles = Article.objects.all()
+
+    context = {
+        'message': 'Delete Article' + str(id),
+        'articles': articles,
+    }
+    return render(request, 'sns/index.html', context)
